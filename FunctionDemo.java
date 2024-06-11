755e3dcbc81c30f11ec8ce5ba2f2766b9ed1e75f
Veda9477 <vedapatil9477@gmail.com>
2024-06-11 14:59:55 +0530

    This file is denoted to calling function with parameter

---
+++ FunctionDemo.java
@@ -0,0 +1,18 @@
+package Day;
+
+public class FunctionDemo {
+	public static void add() {
+		int a = 10;
+		int b = 20;
+		int total = a+b;
+		System.out.println("Addition :"+total);
+	}
+	public static void sub(int a,int b) {
+		//int d = 10;
+		//int e = 20;
+		int total = a-b;
+		System.out.println("Substraction :"+total);
+		
+	}
+
+}
---
+++ Functn.java
@@ -0,0 +1,14 @@
+package Day;
+
+public class Functn {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello");
+		//FunctionDemo fd = new FunctionDemo(); //object creation
+		
+		FunctionDemo.add();
+		FunctionDemo.sub(12,15);
+
+	}
+
+}
